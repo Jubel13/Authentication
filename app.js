4cc014f828c41885cde478a54fcc4d495d50c71a
Jubel13 <jubelsinaga13@gmail.com>
2021-04-22 16:37:32 +0700

    Level 4, bycrypt hash with salting

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// const md5 = require('md5');
 // const encrypt = require('mongoose-encryption')
+
 
 const app = express();
 
@@ -43,22 +46,24 @@ app.get('/register', function(req, res) {
 })
 
 app.post('/register', function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err) {
-    if (!err) {
-      res.render("secrets")
-    } else {
-      console.log(err);
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets")
+      } else {
+        console.log(err);
+      }
+    });
   });
 })
 
 app.post('/login', function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({
     email: username
@@ -67,9 +72,11 @@ app.post('/login', function(req, res) {
       console.log(err);
     } else {
       if (result) {
-        if (result.password === password) {
-          res.render('secrets')
-        }
+        bcrypt.compare(password, result.password, function(err, foundPass) {
+          if (foundPass === true) {
+            res.render('secrets')
+          }
+        });
       }
     }
   })
